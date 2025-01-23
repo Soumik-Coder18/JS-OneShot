24e24e1c2dc88b441de42d665704613afe80d3b3
Soumik-Coder18 <bagsoumik6@gmail.com>
2025-01-23 15:30:59 +0000

    Config Done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Soumik"
+let score2 = "33"
+console.log(typeof score);
+console.log(typeof score2);
+let valueNum1 = Number(score)
+let valueNum2 = Number(score2)
+console.log(valueNum1);
+console.log(valueNum2);
+
+
+// "33" => 33
+// "33ajgawd" => NaN
+// true = 1 and false = 2
+
+let isLogged = 1
+let name = "Soumik"
+let booleanConvert = Boolean(isLogged)
+let Soumik1 = Boolean(name)
+console.log(booleanConvert);
+console.log(Soumik1);
+
+// 1 => true and 0 => false 
+// " " => false 
+// "String" => true
+
+let num = 45
+let sringNum = String(num)
+console.log(sringNum);
+console.log(typeof sringNum)
