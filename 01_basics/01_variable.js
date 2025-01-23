4b16a3837ae85ac6a43153258278507d7d0738ea
Soumik-Coder18 <bagsoumik6@gmail.com>
2025-01-23 10:41:58 +0000

    config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 17723
+let accountEmail = "bagsoumik6@gmail.com"
+var accountPsw = "1234"
+accountCity = "Kolkata"
+console.table([accountEmail,accountId,accountPsw,accountCity])
+// accountId = 23 //Not Allowed 
+accountEmail = "awgaf@gamil.com"
+accountPsw = "2547"
+accountCity = "Jaipur"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPsw,accountCity])
+/*prefer not to use var */
+
+
+let accountState; //Undefined
+console.log(accountState);
