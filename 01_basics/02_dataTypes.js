2ade52d31312958529cd62a775cab88d9344be21
Soumik-Coder18 <bagsoumik6@gmail.com>
2025-01-23 10:51:59 +0000

    config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // We cann't use it in nodeJs
+
+let name ="Soumik"
+let age = 12
+let isLogged = true 
+
+// number => 2^53
+//bigint 
+//string => "Soumik"
+//boolean => true / false 
+//null => Standalone 
+//undefined 
+//symbol => unique
+
+
+// object
+
+console.log(typeof name);
+console.log(typeof age);
+
