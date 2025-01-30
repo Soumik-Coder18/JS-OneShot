af60e0a59eb15a132469a3906fad17cf6ded4d8f
Soumik-Coder18 <bagsoumik6@gmail.com>
2025-01-30 10:35:17 +0000

    😀

---
+++ 03_basics/06_iife.js
@@ -0,0 +1,9 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function name(){
+    console.log('Soumik');
+})();//here ; is important //First () is for declaration and second () is for call
+
+((name)=>{
+    console.log(name,"Happy Birthday to you!!");
+})("Soumik");
